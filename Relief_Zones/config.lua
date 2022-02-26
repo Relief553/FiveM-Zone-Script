e049d6cebc16c62c941ecbfc11b21784a1be9642
Relief Development <100314895+Relief553@users.noreply.github.com>
2022-02-25 22:40:34 -0500

    Add files via upload

---
+++ Relief_Zones/config.lua
@@ -0,0 +1,81 @@
+--					 ===================================
+--					 Copyright © 2022 Relief Development
+--					 ===================================
+
+config = {
+	
+	-- Config Help --
+-- {
+--	coords = {x = , y = , z = },
+--	zone = {radius = 70.0, color = 38}, 
+--	blip = {draw = true, id = 0, color = 38, text = "Text"}
+-- },
+-- Coords are were you want the blip so go to the location you want and get the x, y, z coords and import them
+-- Zone radius is equal to the size of the blip and color number is equal to a number witch can be found here https://docs.fivem.net/docs/game-references/blips/
+-- Text is what the blip says on the map when someone whovers over it draw is if the blip spawns in or not and id is the type of blip witch can be found here https://docs.fivem.net/docs/game-references/blips/
+
+-- Custon Zones -- 
+	zones = {
+		{
+			coords = {x = 1844.39, y = 3685.97, z = 41.5},
+			zone = {radius = 70.0, color = 38}, 
+			blip = {draw = true, id = 60, color = 38, text = "Blaine County Sheriff's Office"}
+		},
+		{
+			coords = {x = 1689.1, y = 3587.46, z = 40.32}, 
+			zone = {radius = 50.0, color = 1}, 
+			blip = {draw = true, id = 60, color = 1, text = "Sandy Shores Fire Department"}
+		},
+		{
+			coords = {x = 1676.32, y = 2587.62, z = 61.09}, 
+			zone = {radius = 200.0, color = 5}, 
+			blip = {draw = true, id = 60, color = 5, text = "Blaine Couinty Prison"}
+		},
+		{
+			coords = {x= -445.63, y = 6007.84, z = 40.41 ,},
+			zone = {radius = 100.0, color = 38},
+			blip = {draw = true, id = 60, color = 38, text = "Paleto Bay Sheriff's Office"}
+		},
+		{
+			coords = {x= -370.29, y = 611.33, z = 39.46,},
+			zone = {radius = 50.0, color = 1},
+			blip = {draw = true, id = 60, color = 1, text = "Paleto Bay Fire Department"}
+		},
+		{
+			coords = {x= 441.91, y = 985.3, z = 43.69,},
+			zone = {radius = 100.0, color = 38},
+			blip = {draw = true, id = 60, color = 38, text = "Mission Row Police Department"}
+		},
+		{
+			coords = {x= -1086.22, y = -809.47, z = 31.26,},
+			zone = {radius = 50.0, color = 38},
+			blip = {draw = true, id = 60, color = 38, text = "Rockford Hills Police Station"}
+		},
+		{
+			coords = {x= 630.97, y = -1.27, z = 99.42,},
+			zone = {radius = 50.0, color = 38},
+			blip = {draw = true, id = 60, color = 38, text = "Vinewood Hills Police Station"}
+		},
+		{
+			coords = {x= 378.06, y = -1606.37, z = 36.95,},
+			zone = {radius = 50.0, color = 38},
+			blip = {draw = true, id = 60, color = 38, text = "Innocence Blvd Polce Station"}
+		},
+		{
+			coords = {x= -637.02, y = -115.06, z = 38.02,},
+			zone = {radius = 50.0, color = 1},
+			blip = {draw = true, id = 60, color = 1, text = "Rockford Hills Fire Department"}
+		},
+		{
+			coords = {x= 1197.18, y = -1464.93, z = 44.09,},
+			zone = {radius = 50.0, color = 1},
+			blip = {draw = true, id = 60, color = 1, text = "Capital Blvd Fire Department"}
+		},
+		{
+			coords = {x= 211.77, y = -1643.79, z = 40.39,},
+			zone = {radius = 50.0, color = 1},
+			blip = {draw = true, id = 60, color = 1, text = "Macdonald Blvd Fire Department"}
+		}
+	}
+	
+}
---
+++ Relief_Zones/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Relief Development'
+description 'Zone script for FiveM'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    './client/client.lua'
+}
